469bad7c9b01bc054f8e38668b628f79f20f1cdb
Joshua Snider <josh@joshuasnider.com>
2016-01-30 10:47:11 -0500

    Sexy primes

---
+++ src/main/java/SexyPrimeServlet.java
@@ -0,0 +1,62 @@
+package com.joshuasnider.ecc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.json.JsonArray;
+import javax.servlet.http.*;
+
+/**
+ * List sexy primes.
+ */
+public class SexyPrimeServlet extends CommonServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      resp.setContentType("aplication/json");
+      resp.getWriter().print("{ \"data\": [");
+      JsonArray arr = getUnnamedArgs(req);
+      List<Integer> primes = primesLessThan(arr.getInt(0));
+      List<String> pairs = new ArrayList<String>();
+      for (int prime : primes) {
+        if (primes.contains(prime + 6)) {
+          pairs.add("[" + prime + ", " + (prime + 6) + "]");
+        }
+      }
+      for (int index = 0; index < pairs.size(); index++) {
+        resp.getWriter().print(pairs.get(index));
+        if (index != pairs.size() - 1) {
+          resp.getWriter().print(", ");
+        }
+      }
+      resp.getWriter().println("] }");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      doGet(req, resp);
+    }
+
+    public boolean isPrime(int num) {
+      for (int x = 2; x < num; x++) {
+        if (num % x == 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    public List<Integer> primesLessThan(int max) {
+      List<Integer> primes = new ArrayList<Integer>();
+      for (int x = 2; x < max; x++) {
+        if (isPrime(x)) {
+          primes.add(x);
+        }
+      }
+      return primes;
+    }
+}
+
