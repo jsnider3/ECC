51f5b0c11599b7deff61cc9f3d86da1ba968b7c2
Joshua Snider <josh@joshuasnider.com>
2016-01-30 10:13:05 -0500

    Palindromes

---
+++ src/main/java/PalindromeServlet.java
@@ -0,0 +1,44 @@
+package com.joshuasnider.ecc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.json.JsonArray;
+import javax.servlet.http.*;
+
+/**
+ * Is something an anagram?
+ */
+public class PalindromeServlet extends CommonServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      resp.setContentType("aplication/json");
+      resp.getWriter().print("{ \"data\": [");
+      JsonArray arr = getUnnamedArgs(req);
+      for (int count = 0; count < arr.size(); count++) {
+        if (isPalindrome(arr.getString(count))) {
+          resp.getWriter().print("true");
+        } else {
+          resp.getWriter().print("false");
+        }
+        if (count != arr.size() - 1) {
+          resp.getWriter().print(", ");
+        }
+      }
+      resp.getWriter().println("] }");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      doGet(req, resp);
+    }
+
+    public boolean isPalindrome(String first) {
+      return first.equals(new StringBuilder(first).reverse().toString());
+    }
+}
+
