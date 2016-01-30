e195b3cbe31e1dcd2a74b2b81e0c9f193aa896f9
Joshua Snider <josh@joshuasnider.com>
2016-01-30 10:20:06 -0500

    Broken hello.

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -7,28 +7,14 @@ import javax.servlet.http.*;
 /**
  * Prints hello world repeated.
  */
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends CommonServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
       resp.setContentType("aplication/json");
       resp.getWriter().print("{ \"data\": [");
-      int reps = 5;
-      //
-      try {
-        String line = null;
-        StringBuffer data = new StringBuffer();
-        BufferedReader reader = req.getReader();
-        while ((line = reader.readLine()) != null) {
-          data.append(line);
-        }
-        String stringed = data.toString();
-        reps = Integer.parseInt(
-          stringed.substring(1, stringed.length() - 1));
-      }
-      catch (Exception e) {}
-      //
+      int reps = getUnnamedArgs(req).getInt(0);
       String hello = "\"Hello World\"";
       for (int count = 0; count < reps; count++) {
         resp.getWriter().print(hello);
